1c4e9b7286d0c52266af2718ca84e73a052b41b3
Blaakmeer, Robert <robert.blaakmeer@philips.com>
2017-11-08 08:47:26 +0100

    added script to activate bluetooth

---
+++ activate-bluetooth.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Activating bluetooth on RPI ZeroW. Make sure you have a bluetooth device enabled and visible."
+
+echo "Attaching hci0..."
+if ! /usr/bin/hciattach /dev/ttyAMA0 bcm43xx 921600 noflow -; then
+    echo "First try failed. Let's try another time."
+    /usr/bin/hciattach /dev/ttyAMA0 bcm43xx 921600 noflow -
+fi
+
+echo "Bring hci0 up..."
+hciconfig hci0 up
